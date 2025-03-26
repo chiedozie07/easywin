199b72c51d566dfbb343e2ea14b3b90be1ae88e6
skillztec <skillztech19@gmail.com>
2025-03-27 00:57:58 +0100

    Added PageNotFound router component

--- src/router.tsx
+++ src/router.tsx
@@ -3,9 +3,10 @@ import HomePage from "./pages/HomePage";
 import App from "./App";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
+import PageNotFound from "./components/PageNotFound";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <App /> },
+  { path: "/", element: <App />, errorElement: <PageNotFound />, },
   { path: "/register", element: <RegisterPage /> },
   { path: "/login", element: <LoginPage /> },
 
